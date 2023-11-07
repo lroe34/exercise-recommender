daed1a2845fb838d2f0da55aca9e2198f756e0ed
Luke Roe <101296666+lroe34@users.noreply.github.com>
2023-11-07 16:00:50 -0600

    added reps

--- server/src/app.py
+++ server/src/app.py
@@ -13,7 +13,7 @@ def process_form():
     muscle_group = request.form.get('muscleGroup')
     skill_level = request.form.get('skillLevel')
     goal = request.form.get('goal')
-    user_profile = f'{{"recommendedperweek": "2-3", "musclegroup": "{muscle_group}", "difficulty": "{skill_level}", "goal": "{goal}"}}'
+    user_profile = f'{{"recommendedperweek": "2-3", "musclegroup": "{muscle_group}", "difficulty": "{skill_level}"}}'
     # Process the form data here
     # read from the txt file
     with open('ex.txt', 'r') as file:
@@ -22,6 +22,11 @@ def process_form():
         contents = contents.replace("```", "")
         contents = contents.replace("json", "")
 
+    reps = {
+        "strength": "4-8 reps",
+        "hypertrophy": "8-12 reps",
+        "endurance": "15-20 reps"
+    }
 
     workouts = contents.split(", ")
     tfidf_vectorizer = TfidfVectorizer()
@@ -32,12 +37,19 @@ def process_form():
     recommendations = sorted(enumerate(cosine_similarities[0]), key=lambda x: x[1], reverse=True)[:6]
 
     workout_details = []
+    
+
     for rec in recommendations:
+        
         full_recommendation = workouts[rec[0]]
         workout_name = full_recommendation.split(":")[0][1:].title().replace("{", " ")
         per_week = full_recommendation.split(":")[2].split(',')[0]
         muscle_group = full_recommendation.split(":")[3].title().split(',')[:-1]
         difficulty = full_recommendation.split(":")[4].replace("difficulty:", "").replace("}", "")
-        workout_details.append((workout_name, per_week, muscle_group, difficulty))
+
+        if "plank" in workout_name.lower():
+            workout_details.append((workout_name, per_week, muscle_group, difficulty,""))
+        else:
+            workout_details.append((workout_name, per_week, muscle_group, difficulty,reps[goal]))
 
     return render_template('index.html', recommended=workout_details)
